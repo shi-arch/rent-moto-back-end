c2586dc96f51a2462599e27e95a393bc209968c9
Shivram kashyap <kashyapshivram512@gmail.com>
2024-08-17 17:35:58 +0530

    aa

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://rent-moto-front-end.vercel.app', 'https://vibezone.in']
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://rent-moto-front-end.vercel.app']
   },
 });
 const startServer = async () => {
